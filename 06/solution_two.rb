b48b78c5dfb134cc3f6fd527f6972e20a2646234
Karol Stepniewski <kstepniewski@netflix.com>
2023-12-18 12:42:12 -0800

    Day 6 part 2

--- 06/solution_two.rb
+++ 06/solution_two.rb
@@ -5,9 +5,9 @@ def distance_traveled(pressed_time, race_time)
 end
 
 data = File.readlines(INPUT_FILE)
-time = data[0].split(':')[1].strip.split(' ').map(&:to_i)
-distance = data[1].split(':')[1].strip.split(' ').map(&:to_i)
-races = time.zip(distance)
+time = data[0].split(':')[1].strip.split(' ').join.to_i
+distance = data[1].split(':')[1].strip.split(' ').join.to_i
+races = [[time, distance]]
 
 result = races.map do |race|
   (1..race[0]).map { |pressed_time| distance_traveled(pressed_time, race[0]) }.select { |distance| distance > race[1] }.count
