9cefd1b6576159cccbb9ba42892ef4763309607e
Karol Stepniewski <kstepniewski@netflix.com>
2023-12-19 12:23:53 -0800

    Day 10 part 2 cleanup

--- 10/solution_two.rb
+++ 10/solution_two.rb
@@ -30,10 +30,11 @@ class Tile
     @directions << 'R' if right && right.directions.include?('L')
     @directions << 'U' if up && up.directions.include?('D')
     @directions << 'D' if down && down.directions.include?('U')
+    @shape = shape_from_dirs
   end
 
   def shape_from_dirs
-    ALLOWED_DIRS.map {|k, v| [v.sort, k]}.to_h[directions.sort]
+    ALLOWED_DIRS.map { |k, v| [v.sort, k] }.to_h[directions.sort]
   end
 
   def neighbours
@@ -86,25 +87,21 @@ class Board
 
   def graph
     return @graph if @graph
-    @graph = []
+    @graph = {}
     start_tile.init_start
     to_visit = [start_tile]
     until to_visit.empty?
       visiting = to_visit.shift
-      @graph << visiting
-      neighbours = visiting.neighbours
-      neighbours.each do |neigh|
-        if neigh.distance < 0 || neigh.distance > (visiting.distance + 1)
-          neigh.distance = visiting.distance + 1
-          to_visit << neigh
-        end
-      end
+      @graph[visiting] = true
+      visiting.neighbours
+              .select { |neigh| neigh.distance < 0 || neigh.distance > visiting.distance + 1 }
+              .each { |neigh| neigh.distance = visiting.distance + 1; to_visit << neigh }
     end
     @graph
   end
 
   def max_distance
-    graph.max_by(&:distance).distance
+    graph.keys.max_by(&:distance).distance
   end
 
   def [](x, y)
@@ -113,73 +110,68 @@ class Board
   end
 
   def tiles_inside
-    inside = []
+    inside = {}
+    outside = {}
     (0...$width).each do |x|
-      (0...$height).each do |y|
-        tile = self[x,y]
-        next if graph.include?(tile)
-        inside << tile if is_inside?(tile)
+      (0...$height).reverse_each do |y|
+        tile = self[x, y]
+        next if graph.has_key?(tile)
+        if is_inside?(tile, inside, outside, graph)
+          inside[tile] = true
+        else
+          outside[tile] = true
+        end
       end
     end
-    inside
+    inside.keys
   end
 
-  def is_inside?(tile)
-    in_ray = graph.select { |gtile| gtile.x == tile.x && gtile.y > tile.y }.sort_by(&:y)
-    return false if in_ray.empty?
+  def is_inside?(tile, inside, outside, graph)
     crossings = 0
     turn_seen = nil
-    in_ray.each do |tile|
-      pipe = tile.shape
-      pipe = tile.shape_from_dirs if tile.shape == 'S'
-      if pipe == '-'
-        crossings += 1
-        next
-      end
+    until tile.nil?
+      return true if inside.has_key?(tile) && crossings == 0
+      return false if outside.has_key?(tile) && crossings == 0
 
-      if pipe == 'J'
-        if turn_seen == 'F'
+      if graph.has_key?(tile)
+        case tile.shape
+        when '-'
           crossings += 1
-          turn_seen = nil
-        else
-          turn_seen = pipe
+        when 'J'
+          if turn_seen == 'F'
+            crossings += 1
+            turn_seen = nil
+          else
+            turn_seen = tile.shape
+          end
+        when 'F'
+          if turn_seen == 'J'
+            crossings += 1
+            turn_seen = nil
+          elsif turn_seen == 'L'
+            turn_seen = nil
+          else
+            turn_seen = tile.shape
+          end
+        when '7'
+          if turn_seen == 'L'
+            crossings += 1
+            turn_seen = nil
+          elsif turn_seen == 'J'
+            turn_seen = nil
+          else
+            turn_seen = tile.shape
+          end
+        when 'L'
+          if turn_seen == '7'
+            crossings += 1
+            turn_seen = nil
+          else
+            turn_seen = tile.shape
+          end
         end
-        next
       end
-
-      if pipe == 'F'
-        if turn_seen == 'J'
-          crossings += 1
-          turn_seen = nil
-        elsif turn_seen == 'L'
-          turn_seen = nil
-        else
-          turn_seen = pipe
-        end
-        next
-      end
-
-      if pipe == '7'
-        if turn_seen == 'L'
-          crossings += 1
-          turn_seen = nil
-        elsif turn_seen == 'J'
-          turn_seen = nil
-        else
-          turn_seen = pipe
-        end
-        next
-      end
-
-      if pipe == 'L'
-        if turn_seen == '7'
-          crossings += 1
-          turn_seen = nil
-        else
-          turn_seen = pipe
-        end
-        next
-      end
+      tile = tile.up
     end
     crossings.odd?
   end
@@ -188,5 +180,4 @@ end
 $board = Board.new
 
 puts $board.max_distance
-puts $board.graph.length
 puts $board.tiles_inside.count
