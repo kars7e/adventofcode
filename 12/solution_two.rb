6dd4cc4d0d379e14c5e8df66cf46cc5cc789b69b
Karol Stepniewski <kstepniewski@netflix.com>
2023-12-19 17:30:58 -0800

    Day 12 part 2

---
+++ 12/solution_two.rb
@@ -0,0 +1,46 @@
+example = false
+file = example ? 'example' : 'input'
+INPUT_FILE = __dir__ + "/#{file}.txt"
+
+rows = File.readlines(INPUT_FILE).map{ |row| row.split(' ') }
+patterns = rows.map(&:first)
+sequences = rows.map(&:last).map{|sequence| sequence.split(',').map(&:to_i)}
+
+$block_memo = {}
+$memo = {}
+
+def subproblem(pattern, sequences)
+  return $memo[[pattern, sequences]] if $memo[[pattern, sequences]]
+  if sequences.empty?
+    return 0 if pattern && pattern.include?('#')
+    return 1
+  end
+  return 0 if pattern.nil? || pattern.empty?
+  idx = find_block(pattern, sequences[0])
+  return 0 if idx.nil?
+  remainder = pattern[idx+sequences[0]+1..]
+  result = subproblem(remainder, sequences[1..])
+  result += subproblem(pattern[idx + 1..], sequences) if pattern[idx] == '?'
+  $memo[[pattern, sequences]] = result
+  result
+end
+
+def find_block(pattern, length)
+  $block_memo[[pattern, length]] ||= find_first_block(pattern, length)
+end
+
+def find_first_block(pattern, length)
+  pattern.chars.each_cons(length).with_index do |block, index|
+    return index if block.all? { |elem| elem == '#' || elem == '?' } && pattern[index+length] != '#'
+    return nil if block[0] == '#'
+  end
+  nil
+end
+
+
+
+solution = (0...rows.length).map do |index|
+  subproblem(([patterns[index]]*5).join('?'), sequences[index]*5)
+end.sum
+
+puts solution
